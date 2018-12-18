c1378e5d96a839a0d3f544ad21c0393cd2156d03
rioirinko <lina.rio@mail.ru>
2018-12-19 01:21:17 +0600

    docker

--- crm/users/views.py
+++ crm/users/views.py
@@ -1,15 +1,16 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegistrationSerializer, LoginSerializer
 
 
-class RegistrationAPIView(APIView):
+class RegistrationAPIView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
 
-    def post(self, request):
+    def post(self, request, **kwargs):
         user = request.data
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
@@ -17,11 +18,11 @@ class RegistrationAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class LoginAPIView(APIView):
+class LoginAPIView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = LoginSerializer
 
-    def post(self, request):
+    def post(self, request, **kwargs):
         user = request.data
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
